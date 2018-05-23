b174a9f21538bfd540db2a5feebe3a25292a011c
db0company <db0company@gmail.com>
2018-05-23 12:14:37 +0900

    Event modification form javascript: generic way to show/hide fields per versions using variables passed from python

--- js/main.js
+++ js/main.js
@@ -1,41 +1,24 @@
-function onVersionChange(animation) {
-//for JP
-       if ($('.container-for-generic-form #id_c_versions_0').val() == true) {
-
-           $('.container-for-generic-form #id_jp_banner').closest('.form-group').show(animation);
-	   $('.container-for-generic-form #id_jp_start_date').closest('.form-group').show(animation);
-           $('.container-for-generic-form #id_jp_end_date').closest('.form-group').show(animation);
-        }
-       else {
-           $('.container-for-generic-form #id_jp_banner').closest('.form-group').hide(animation);
-	   $('.container-for-generic-form #id_jp_start_date').closest('.form-group').hide(animation);
-           $('.container-for-generic-form #id_jp_end_date').closest('.form-group').hide(animation);
-
-           $('.container-for-generic-form #id_jp_banner').prop('value', "");
-	   $('.container-for-generic-form #id_jp_start_date').prop('value', "");
-	   $('.container-for-generic-form #id_jp_end_date').prop('value', "");
-        }
-//for WW
-       if ($('.container-for-generic-form #id_c_versions_1').val() == true) {
-           $('.container-for-generic-form #id_ww_banner').closest('.form-group').show(animation);
-	   $('.container-for-generic-form #id_ww_start_date').closest('.form-group').show(animation);
-           $('.container-for-generic-form #id_ww_end_date').closest('.form-group').show(animation);
-        }
-       else {
-           $('.container-for-generic-form #id_ww_banner').closest('.form-group').hide(animation);
-	   $('.container-for-generic-form #id_ww_start_date').closest('.form-group').hide(animation);
-           $('.container-for-generic-form #id_ww_end_date').closest('.form-group').hide(animation);
-//I don't think banner and start dates have a 'checked' property, and I didn't see anything else
-//that might be relevant other than value for dates (i have no clue about banner) so yea
-           $('.container-for-generic-form #id_ww_banner').prop('value', "");
-	   $('.container-for-generic-form #id_ww_start_date').prop('value', "");
-	   $('.container-for-generic-form #id_ww_end_date').prop('value', "");
-        }
-//Lather rinse repeat for all versions once this works 0w0
+function onVersionChange(form, animation) {
+    if (typeof version_prefixes == 'undefined' || typeof fields_per_version == 'undefined'
+        || !version_prefixes || !fields_per_version) {
+        return;
+    }
+    $.each(version_prefixes, function(version, prefix) {
+        let checkbox = form.find('#id_c_versions [value="' + version + '"]');
+        $.each(fields_per_version, function(i, field_name) {
+            let input = form.find('#id_' + prefix + field_name);
+            let field = input.closest('.form-group');
+            if (checkbox.prop('checked')) {
+                field.show(animation);
+            } else {
+                field.hide(animation);
+                input.val('');
+            }
+        });
+    });
 }
 function loadVersions() {
-        onVersionChange();
-//I thought these would be specific enough selectors, but if they're wrong lemme know
-        $('.container-for-generic-form #id_c_versions').change(function () { onVersionChange('slow') });
+    let form = $('[data-form-name$="_event"]');
+    onVersionChange(form);
+    form.find('#id_c_versions').change(function () { onVersionChange(form, 'slow') });
 }
-
